910b66cc9d01af9da4f65c7098290c4075cc5cde
Evgeniy Andreev (gsomix) <gsomix@gmail.com>
2019-04-19 00:43:21 +0400

    Implement MVU pattern

--- Gyges/Game.fs
+++ Gyges/Game.fs
@@ -1,14 +1,35 @@
 namespace Gyges
 
 open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Content
 open Microsoft.Xna.Framework.Graphics;
+open Microsoft.Xna.Framework.Input
 
-type GameLoop() =
+type InputState =
+    {
+        Keyboard: KeyboardState
+    }
+    
+type Config =
+    {
+        Width: int
+        Height: int
+    }
+
+type GameLoop<'Model, 'Content>( config: Config,
+                                 loadContent: ContentManager -> 'Content,
+                                 init: unit -> 'Model,
+                                 update: InputState -> 'Model -> 'Model,
+                                 draw: SpriteBatch -> 'Content -> 'Model -> unit ) =
     inherit Game()
 
-    let mutable spriteBatch = Unchecked.defaultof<SpriteBatch>
-    let mutable fpsFont = Unchecked.defaultof<SpriteFont>
-
+    let mutable renderTarget: RenderTarget2D = null
+    let mutable spriteBatch: SpriteBatch = null
+    
+    let mutable model = Unchecked.defaultof<'Model>
+    let mutable content = Unchecked.defaultof<'Content>
+    
+    let mutable fpsFont: SpriteFont = null
     let mutable fps = 0
     let mutable lastFpsUpdate = 0.
     let fpsUpdateInterval = 100.
@@ -24,17 +45,48 @@ type GameLoop() =
               position = Vector2(20.0f, 20.0f),
               color = Color.White )
 
-
-    override this.LoadContent() = 
+    override this.Initialize() =
+        renderTarget <- new RenderTarget2D(this.GraphicsDevice, config.Width, config.Height)
         spriteBatch <- new SpriteBatch(this.GraphicsDevice)
+        model <- init()
+        base.Initialize()
+
+    override this.LoadContent() =
         fpsFont <- this.Content.Load<SpriteFont> "./connection"
+        content <- loadContent (this.Content)
+        base.LoadContent()
 
-    override __.Update(gameTime) = ()
+    override __.Update(gameTime) =
+        let input: InputState = { Keyboard = Keyboard.GetState() }
+        model <- update input model
+        base.Update(gameTime)
 
-    override this.Draw(gameTime) = 
-        this.GraphicsDevice.Clear Color.Black
+    override this.Draw(gameTime) =
+        let gd = this.GraphicsDevice
+        gd.Clear Color.Black
+        
+        gd.SetRenderTarget(renderTarget)
+        draw spriteBatch content model
+        
+        let screenWidth = gd.PresentationParameters.BackBufferWidth;
+        let screenHeight = gd.PresentationParameters.BackBufferHeight;
+        
+        let aspect = config.Width/config.Height
+        
+        let rect =
+            if screenWidth > screenHeight
+            then
+                let actualWidth = aspect*screenHeight
+                let topLeft = (screenWidth - actualWidth) / 2
+                new Rectangle(topLeft, 0, actualWidth, screenHeight)
+            else
+                let actualHeight = screenWidth/aspect
+                let topLeft = (screenHeight - actualHeight) / 2
+                new Rectangle(0, topLeft, screenWidth, actualHeight)
+        
+        gd.SetRenderTarget(null)
         spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp)
+        spriteBatch.Draw(renderTarget, rect, Color.White)
         updateAndPrintFPS gameTime spriteBatch
         spriteBatch.End()
-
-
+        base.Draw(gameTime)
--- Gyges/Program.fs
+++ Gyges/Program.fs
@@ -1,17 +1,37 @@
 ﻿open Gyges
 
 open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Content
+open Microsoft.Xna.Framework.Graphics
+
+type Content = { Ship: Texture2D }
+    
+let loadContent (contentManager: ContentManager): Content =
+    { Ship = contentManager.Load("ship") }
+
+type Model = { Pos: Vector2 }
+    
+let init(): Model = { Pos = Vector2(50.0f, 50.0f) }
+    
+let update (input: InputState) (model: Model): Model = model
+
+let draw (spriteBatch: SpriteBatch) (content: Content) (model: Model) =
+    spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp)
+    spriteBatch.Draw(content.Ship, model.Pos, Color.White)
+    spriteBatch.End()
+
+let config: Config = { Width = 128; Height = 128 }
 
 [<EntryPoint>]
 let main argv =
-    use game = new GameLoop()
+    use game = new GameLoop<Model, Content>(config, loadContent, init, update, draw)
     game.IsFixedTimeStep <- false
 
     use graphics = new GraphicsDeviceManager(game)
-    graphics.IsFullScreen <- true
+    graphics.IsFullScreen <- false
     graphics.SynchronizeWithVerticalRetrace <- false
-    graphics.PreferredBackBufferWidth <- 800
-    graphics.PreferredBackBufferHeight <- 600
+    graphics.PreferredBackBufferWidth <- 1000      
+    graphics.PreferredBackBufferHeight <- 640
     graphics.ApplyChanges()
     
     game.Run()
